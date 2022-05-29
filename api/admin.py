9293a7097c9a374ac4fb3c26fb13fab2696e2938
Ainur Gimadeev <inuur@mail.ru>
2022-05-29 18:20:16 +0300

    add Post model & fix Blog model constraints

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Blog
+from .models import (
+    Blog,
+    Post,
+)
 
-# Register your models here.
 admin.site.register(Blog)
+admin.site.register(Post)
--- api/models.py
+++ api/models.py
@@ -12,7 +12,22 @@ class Blog(models.Model):
     followers = models.ManyToManyField(
         User,
         related_name='followed_blogs',
+        blank=True
     )
 
     def __str__(self):
         return f"{self.author.username}'s blog"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField(max_length=140, blank=True, default='')
+    created_time = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+    def __str__(self):
+        return self.title
