9a773ff7a9bb1cbf128d9f944f31e99315395f48
Ainur Gimadeev <inuur@mail.ru>
2022-05-29 19:26:28 +0300

    show liked posts of User in admin panel

--- user/admin.py
+++ user/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Liked posts',
+            {
+                'fields': (
+                    'liked_posts',
+                )
+            }
+        )
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    liked_posts = models.ManyToManyField(
+        'api.Post',
+        blank=True
+    )
