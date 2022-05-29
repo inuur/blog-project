9a4df3c62c8645f430614ca0513d0a93e5550f9e
Ainur Gimadeev <inuur@mail.ru>
2022-05-29 18:15:16 +0300

    add Blog model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    author = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='author'
+    )
+    followers = models.ManyToManyField(
+        User,
+        related_name='followed_blogs',
+    )
+
+    def __str__(self):
+        return f"{self.author.username}'s blog"
