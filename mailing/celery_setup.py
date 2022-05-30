06b582577734a62b9690b06e773f8f5c69d29654
Ainur Gimadeev <inuur@mail.ru>
2022-05-30 23:46:55 +0300

    fix celery broke django_setup

--- mailing/celery_setup.py
+++ mailing/celery_setup.py
@@ -1,9 +1,7 @@
 import os
-import django
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
-django.setup()
 
 app = Celery('blog')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- mailing/task.py
+++ mailing/task.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-from user.models import User
-from api.models import Post
 
 
 # Sends email for 500 users each 1 minute, so it won't take too loong
 @shared_task
 def send_email():
+    from user.models import User
+    from api.models import Post
     users = User.objects.filter().all()
     for user in users[:500]:
         last_posts = Post.objects.filter(
