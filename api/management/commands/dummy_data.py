052b7f77a1e057663221a975401d734b4023062d
Ainur Gimadeev <inuur@mail.ru>
2022-05-30 18:44:39 +0300

    test data generation impl

--- api/management/commands/dummy_data.py
+++ api/management/commands/dummy_data.py
@@ -1,4 +1,7 @@
+import random
+
 from django.core.management import BaseCommand
+from mixer.backend.django import mixer
 
 
 class Command(BaseCommand):
@@ -8,4 +11,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        pass
+        superuser = mixer.blend('user.User', username='root', is_superuser=True)
+        superuser.set_password('123')
+        superuser.save()
+        users = mixer.cycle(50000).blend('user.User')
+        users.append(superuser)
+
+        for user in users:
+            blogs = list(map(lambda usr: usr.blog, random.choices(users, k=100)))
+            user.followed_blogs.add(*blogs)
+
+        for _ in range(200000):
+            mixer.blend('api.post', blog=random.choice(users).blog)
